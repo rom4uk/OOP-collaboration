532800a6c20da8ac4a9b65d1b26b46e69d6e828c
Volodymyr <astart7@ukr.net>
2017-06-17 21:02:46 +0300

    Create FPS.js

---
+++ scripts/FPS.js
@@ -0,0 +1,27 @@
+//Рахує FPS та виводить в консоль.
+
+let frameCount = function _fc(timeStart){
+        
+        let now = performance.now();
+        let duration = now - timeStart;
+        
+        if(duration < 1000){
+            
+            _fc.counter++;
+            
+        } else {
+                      
+            _fc.fps = _fc.counter;
+            _fc.counter = 0;
+            timeStart = now; 
+            console.log(_fc.fps);
+        
+
+        }
+        requestAnimationFrame(() => frameCount(timeStart)); 
+    }
+
+frameCount.counter = 0;
+frameCount.fps = 0;
+
+frameCount(performance.now());
